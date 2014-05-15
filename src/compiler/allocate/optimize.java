3db16a14c3f0b8ff8831de11fd2bb290d150d3ad
edly <411988123@qq.com>
2014-05-15 17:16:43 +0800

    peep hole

--- src/compiler/allocate/optimize.java
+++ src/compiler/allocate/optimize.java
@@ -53,45 +53,34 @@ public class optimize
 				}
 			}
 			
-			if ((quad.get(i) instanceof __Move) && (quad.get(i + 1) instanceof __Move))
+			if (quad.get(i) instanceof __Move && quad.get(i).def() != null)
 			{
 				__Move __a = (__Move) quad.get(i);
-				__Move __b = (__Move) quad.get(i + 1);
 				
-				if (__a.src instanceof __LabelAddress && __b.src instanceof __Mem &&
-					__a.def().temp.copy == ((__Mem) __b.src).base.temp.copy)
+				for (int j = i + 1; j <= i + 10 && j < quad.size(); ++j)
 				{
-					__b.special = true;
-					__b.src = __a.src;
-					//System.out.println("!!!");
-				}
-			}
-			
-			if ((quad.get(i) instanceof __Move) && i + 2 < quad.size() && (quad.get(i + 2) instanceof __Move))
-			{
-				__Move __a = (__Move) quad.get(i);
-				__Move __b = (__Move) quad.get(i + 2);
-				
-				if (__a.src instanceof __LabelAddress && __b.src instanceof __Mem &&
-					__a.def().temp.copy == ((__Mem) __b.src).base.temp.copy)
-				{
-					__b.special = true;
-					__b.src = __a.src;
-					//System.out.println("!!!");
-				}
-			}
-			
-			if ((quad.get(i) instanceof __Move) && i + 3 < quad.size() && (quad.get(i + 3) instanceof __Move))
-			{
-				__Move __a = (__Move) quad.get(i);
-				__Move __b = (__Move) quad.get(i + 3);
-				
-				if (__a.src instanceof __LabelAddress && __b.src instanceof __Mem &&
-					__a.def().temp.copy == ((__Mem) __b.src).base.temp.copy)
-				{
-					__b.special = true;
-					__b.src = __a.src;
-					//System.out.println("!!!");
+					if (quad.get(j) instanceof __Branch) break;
+					if (quad.get(j) instanceof __Jump) break;
+					if (quad.get(j) instanceof __Return) break;
+					if (quad.get(j).def() != null && quad.get(j).def().temp.copy == __a.def().temp.copy) break;
+					
+					if (quad.get(j) instanceof __Move)
+					{
+						__Move __b = (__Move) quad.get(j);
+						
+						if (__a.src instanceof __LabelAddress && __b.src instanceof __Mem &&
+							__a.def().temp.copy == ((__Mem) __b.src).base.temp.copy)
+						{
+							__b.special = true;
+							__b.src = __a.src;
+						}
+						
+						if (__a.src instanceof __Const && __b.src instanceof __TempOprand &&
+							__a.def().temp.copy == ((__TempOprand) __b.src).temp.copy)
+						{
+							__b.src = __a.src;
+						}
+					}
 				}
 			}
 			
